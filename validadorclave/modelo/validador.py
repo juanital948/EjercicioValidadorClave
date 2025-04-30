5e95ab18af0946c2c1586a6a6129556c28165ca0
Juanita legarda <juanitalegardaramirez52@gmail.com>
2025-04-30 13:11:49 -0500

    clase regla ganimeds definir clases restantes, en total son 5

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -11,7 +11,13 @@ class ReglaValidacionGanimedes:
             raise NoCumpleLongitudMinimaError("Debe tener más de 8 caracteres")
         if not any(c.isupper() for c in clave):
             raise NoTieneLetraMayusculaError("Debe tener al menos una letra mayúscula")
-
+        if not any(c.islower() for c in clave):
+            raise NoTieneLetraMinusculaError("Debe tener al menos una letra minúscula")
+        if not any(c.isdigit() for c in clave):
+            raise NoTieneNumeroError("Debe tener al menos un número")
+        if not any(c in "@_#$%" for c in clave):
+            raise NoTieneCaracterEspecialError("Debe tener al menos un carácter especial (@ _ # $ %)")
+        return True
 class ReglaValidacionCalisto:
     pass
 
