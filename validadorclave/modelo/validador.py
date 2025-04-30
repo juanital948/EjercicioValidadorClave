a1178fff0dff625095fa9b4420994fa1c796fb34
Juanita legarda <juanitalegardaramirez52@gmail.com>
2025-04-30 13:10:53 -0500

    clase regla ganimeds definir 2 clases

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,9 +1,19 @@
-from errores import *
+from validadorclave.modelo.errores import NoCumpleLongitudMinimaError, NoTieneLetraMayusculaError, \
+    NoTieneLetraMinusculaError, NoTieneNumeroError, NoTieneCaracterEspecialError
 
-class ReglaGanimedes:
+
+class ReglaValidacion:
+    pass
+
+class ReglaValidacionGanimedes:
     def validar(self, clave):
         if len(clave) <= 8:
             raise NoCumpleLongitudMinimaError("Debe tener más de 8 caracteres")
         if not any(c.isupper() for c in clave):
             raise NoTieneLetraMayusculaError("Debe tener al menos una letra mayúscula")
 
+class ReglaValidacionCalisto:
+    pass
+
+class Validador:
+    pass
